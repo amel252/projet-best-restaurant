9ebd68c9611e43106a326cf813675c9bbb9968ed
Amel <amelghanem25@gmail.com>
2026-07-26 20:05:49 +0200

    Configuration des contraintes avec affichage de message d'erreur

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class LoginController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationsUtils): Response
+    {
+        // avoir l'erreur de connexion si y'en a 
+        $error = $authenticationsUtils->getLastAuthenficationError();
+        //  le dernier lastName entré par le user
+        $lastUsername = $authenticationsUtils->getLastUsername();
+
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'=> $error,
+        ]);
+    }
+}
--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -7,10 +7,12 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-// use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-// use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -20,32 +22,55 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            // ->add('email')
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(
+                        message : 'Veuillez saisir votre adresse email.',
+                    ),
+                    new Email(
+                        message: 'Veuillez saisir une adresse email valide.',
+                    ),
+                ],
+            ])
+
             ->add('plainPassword', PasswordType::class,[
                 //  mapped : false parce que mon form contient plainPassword, donc un champ temporaire du formulaire.
                 'mapped'=> false,
                 
                 'constraints'=>[
-                    new NotBlank(),
+                    new NotBlank(
+                        message : 'Veuillez saisir un mot de passe.'
+                    ),
                     new Length(
-                        min : 4,
-                        max : 30
+                        min : 6,
+                        max : 30,
+                        minMessage: 'Le mot de passe doit contenir au moins 6 caractères.'
+                        
                     )
                 ],
             ])
             ->add('firstName', TextType::class,[
                 'constraints'=>[
+                    new NotBlank(
+                        message: 'Veuillez saisir votre prénom.',
+                    ),
                     new Length(
                         min : 4,
-                        max : 30
+                        max : 30,
+                        minMessage: 'Le prénom doit contenir 4 caractères minimum.'
                     )
                 ],
             ])
             ->add('lastName', TextType::class,[
                 'constraints'=>[
+                    new NotBlank(
+                        message: 'Veuillez saisir votre nom.',
+                    ),
                     new Length(
                         min : 4,
-                        max : 30
+                        max : 30,
+                        minMessage: 'Le nom doit contenir 4 caractères minimum.'
                     )
                 ],
             ])
@@ -55,13 +80,6 @@ class RegisterUserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            //  on précise qu'on veux un email unique
-            // 'constraints'=>[
-            //         new UniqueEntity([
-            //             'entityClass'=>User::class,
-            //             'fields'=> 'email'
-            //         ])
-            //     ],
             'data_class' => User::class,
         ]);
     }
