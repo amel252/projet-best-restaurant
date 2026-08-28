455c822e56de00f5f6b966358f4595df63c6dbec
Amel <amelghanem25@gmail.com>
2026-08-28 17:25:08 +0200

    Organisation des fichiers(profile) & mise a jour style

--- src/Controller/Account/AccountController.php
+++ src/Controller/Account/AccountController.php
@@ -15,80 +15,25 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\Address;
+use App\Repository\AddressRepository;
+use App\Form\AddressUserType;
 
 final class AccountController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profil')]
-    public function index(Request $request ,EntityManagerInterface $entityManager): Response
+
+    // pour eviter de mettre entityMan dans chaque route 
+    private $entityManager ;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $user=$this->getUser();
-        // si user n'est pas connecté , redirige vers login 
-        if(!$user){
-            return $this->redirectToRoute('app_login');
-        }
+        $this->entityManager = $entityManager;
 
-        $profileImage = $request->files->get('profileImage');
-        if($profileImage){
-            $newFilename= uniqid() .'.' . $profileImage->guessExtension();
-            try{
-                $profileImage->move(
-                    //  CHEMIN DE STOCK
-                    $this->getParameter('kernel.project_dir') . '/public/uploads/profile',
-                    $newFilename
-                );
-                //  mettre à jour img 
-                $user->setProfileImage($newFilename);
-                // envoyer les donnée en BD 
-                $entityManager->persist($user);
-                $entityManager->flush();
-                    $this->addFlash(
-                        'success',
-                        "l'image est mise a jour."
-                    );
-
-            } catch(FileException $e){
-                $this->addFlash(
-                    'danger',
-                    'Une erreur est survenue lors du téléchargement de l\image.'
-                );
-            }
-        }
-        return $this->render('profile/index.html.twig',[
-            'user'=> $user,
-        ]);
     }
-    // fonction modif infos profil 
-    #[Route('/profile/modifier-infos', name:'app_profil_edit')]
-    public function edit(Request $request, EntityManagerInterface $entityManager):response
-    {
-        // on récup notre user
-        $user = $this->getUser();
-        $form = $this->createForm(ProfileType::class, $user);
-        $form->handleRequest($request);
-        if($form->isSubmitted()){
-            if($form->isValid()){
-                $entityManager->flush();
-                // envoyé msg notification
-                $this->addFlash(
-                type:'success',
-                message:'vos informations ont été mises à jour'
-            );
-                return $this->redirectToRoute('app_profil');
-        }
-            // Notification d'erreur
-        $this->addFlash(
-            'danger',
-            'Le formulaire contient des erreurs.'
-        );
-
-        }
-        return $this->render('profile/edit_profil.html.twig',[
-            'profilModifForm'=>$form->createView(),
-        ]);
-    }
+    
     // fonction modif mot-de-passe compte
-    #[Route('/profile/modifier-mot-de-passe', name:'app_profil_edit_password')]
-    public function edit_password(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher):response
+    #[Route('/compte/modifier-mot-de-passe', name:'app_account_edit_password')]
+    public function editPassword(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher):response
     {
         //  on cible le user connecté 
         $user=$this->getUser();
@@ -117,9 +62,9 @@ final class AccountController extends AbstractController
                 'Votre mot de passe a été modifié avec succès'
             );
             //  quand tout est ok , redirige vers compte
-            return $this->redirectToRoute('app_profil');
+            return $this->redirectToRoute('app_profile');
         }
-        return $this->render('profile/password_edit.html.twig',[
+        return $this->render('account/password_edit.html.twig',[
             'modifyPwd'=> $form->createView()
         ]);
     }
---
+++ src/Controller/Profile/ProfileController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller\Profile;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use App\Entity\User;
+use App\Form\ProfileType;
+use App\Form\PasswordUserType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\Address;
+use App\Repository\AddressRepository;
+use App\Form\AddressUserType;
+
+final class ProfileController extends AbstractController
+{
+
+   
+    //  route profile 
+    #[Route('/profile', name: 'app_profile')]
+    public function index(Request $request ,EntityManagerInterface $entityManager): Response
+    {
+        $user=$this->getUser();
+        // si user n'est pas connecté , redirige vers login 
+        if(!$user){
+            return $this->redirectToRoute('app_login');
+        }
+
+        $profileImage = $request->files->get('profileImage');
+        if($profileImage){
+            $newFilename= uniqid() .'.' . $profileImage->guessExtension();
+            try{
+                $profileImage->move(
+                    //  CHEMIN DE STOCKAQE
+                    $this->getParameter('kernel.project_dir') . '/public/uploads/profile',
+                    $newFilename
+                );
+                //  mettre à jour img 
+                $user->setProfileImage($newFilename);
+                // envoyer les donnée en BD 
+                $entityManager->persist($user);
+                $entityManager->flush();
+                    $this->addFlash(
+                        'success',
+                        "l'image est mise a jour."
+                    );
+
+            } catch(FileException $e){
+                $this->addFlash(
+                    'danger',
+                    'Une erreur est survenue lors du téléchargement de l\image.'
+                );
+            }
+        }
+        return $this->render('profile/index.html.twig',[
+            'user'=> $user,
+        ]);
+    }
+    // fonction modif infos profil 
+    #[Route('/profile/modifier-infos', name:'app_profile_edit')]
+    public function edit(Request $request, EntityManagerInterface $entityManager):response
+    {
+        // on récup notre user
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            if($form->isValid()){
+                $entityManager->flush();
+                // envoyé msg notification
+                $this->addFlash(
+                type:'success',
+                message:'vos informations ont été mises à jour'
+            );
+                return $this->redirectToRoute('app_profile');
+        }
+            // Notification d'erreur
+        $this->addFlash(
+            'danger',
+            'Le formulaire contient des erreurs.'
+        );
+
+        }
+        return $this->render('profile/edit_profil.html.twig',[
+            'profilModifForm'=>$form->createView(),
+        ]);
+    }
+    
+}
